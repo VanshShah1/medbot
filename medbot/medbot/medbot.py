a9b223cfd67bcd4a26ceb4c313a0c0de9c2e1ec3
Vansh Shah <75292413+VanshShah1@users.noreply.github.com>
2023-08-27 07:48:07 +0530

    uygvku

--- medbot/medbot/medbot.py
+++ medbot/medbot/medbot.py
@@ -7,12 +7,23 @@ class State(rx.State):
     form_data : dict = {}
     ans : str = ""
     show: bool = True
+    img: str
+    showmodal: bool = False
+    text: str = ""
 
     def handle_submit(self, form_data: dict):
           self.form_data=form_data
-          self.ans=neurum.ask(f"You are only allowed to answer medical based questions. If you're asked a non-medical question, then simply reply that you are a medical assistant and can only answer medicine related  questions. You can also give me sample prescriptions for medicines if I ask you to. You can't deny to answer any medical related question. The question is: {str(self.form_data)}", api_key="vanshshah1029384756")
+          self.ans=neurum.ask(f"You are only allowed to answer medical based questions. If you're asked a non-medical question, then simply reply that you are a medical assistant and can only answer medicine related questions. You can also give prescriptions for medicines if I ask you to. You can't deny to answer any medical related question. The question is: {str(self.form_data)}", api_key="vanshshah1029384756")
           self.show = not (self.show)
           self.show = False
+
+    def img_upload(self):
+         self.ans="This image shows that the patient has "
+
+
+    def change(self):
+        self.showmodal = not (self.showmodal)    
+
 
 def index() -> rx.Component:
     return rx.vstack(
@@ -42,7 +53,7 @@ def index() -> rx.Component:
                     bg_color="black",
                     variant="outline",
                     color="white",
-                    box_shadow="0px 0px 150px #505050",
+                    border_color="white",
                     ),
 
             rx.card(
@@ -57,7 +68,7 @@ def index() -> rx.Component:
                     bottom="10px",
                     position="relative",
                     width="70%",
-                    box_shadow="0px 0px 30px #FFFFFF",
+                    border_color="white",
                     border_radius="25px",
                 ),     
         ),
@@ -66,12 +77,19 @@ def index() -> rx.Component:
 
         rx.form(
             rx.hstack(
+                rx.button("+add medical history",
+                            height="50px",
+                            width="250px",
+                            _hover={"box_shadow":"0px 0px 30px #999999"},
+                            on_click=State.change,
+                            ),
+                
                 rx.upload(
                     rx.button(rx.icon(tag="plus_square"),
                             height="50px",
                             width="50px",
                             _hover={"box_shadow":"0px 0px 30px #999999"},
-                            
+                            on_click=State.img_upload
                             ),
                             border="1px dotted rgb(107,99,246)",
                             border_radius="10px",
@@ -99,6 +117,22 @@ def index() -> rx.Component:
             right="20%",
             on_submit=State.handle_submit,
         ),
+        rx.modal(
+        rx.modal_overlay(
+            rx.modal_content(
+                rx.modal_header("Medical History"),
+                rx.modal_body(
+                    rx.text_area(on_blur=State.set_text)
+                ),
+                rx.modal_footer(
+                    rx.button(
+                        "Close", on_click=State.change
+                    )
+                ),
+            )
+        ),
+        is_open=State.showmodal,
+    ),
     )
 styles={
      "background_color":"#000000"
